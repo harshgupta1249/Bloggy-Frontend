f9afd50b4e975851e89ec65b967fdb684a8d691a
Harsh <harshgupta1249@gmail.com>
2021-10-23 15:49:16 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
+import Login from './pages/Login/Login';
 import Setting from './pages/Settings/Settings';
 import Single from './pages/Single/Single';
 import Write from './pages/write/Write';
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Setting />
+      <Login />
     </div>
   );
 }
