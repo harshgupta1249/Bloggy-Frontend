7a697bfd013d5a1a69db1e4e7b35d721806890f3
Harsh <harshgupta1249@gmail.com>
2021-10-23 19:52:05 +0530

    route specified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import HomePage from './pages/HomePage/HomePage';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Setting from './pages/Settings/Settings';
-import Single from './pages/Single/Single';
-import Write from './pages/write/Write';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Setting from "./pages/Settings/Settings";
+import Single from "./pages/Single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Register />
-    </div>
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/setting">
+          <Setting />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
