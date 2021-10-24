07d2090dfa4d29dde5a1e067de4dfee5f915d969
Harsh <harshgupta1249@gmail.com>
2021-10-24 13:33:53 +0530

    fetch single post

--- src/components/SinglePost/SignlePost.jsx
+++ src/components/SinglePost/SignlePost.jsx
@@ -1,16 +1,35 @@
+import { useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './SinglePost.css'
 
 const SinglePost = () => {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState({});
+
+
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("http://localhost:8000/post/" + path);
+            setPost(res.data);
+        }
+        getPost();
+    }, [path]);
+
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img
-                    className="singlePostImg"
-                    src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                    alt=""
-                />
+                {post.photo &&
+                    <img
+                        className="singlePostImg"
+                        src={post.photo}
+                        alt=""
+                    />
+                }
+
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
@@ -20,34 +39,13 @@ const SinglePost = () => {
                     <span>
                         Author:
                         <b className="singlePostAuthor">
-                            Safak
+                            {post.username}
                         </b>
                     </span>
-                    <span>1 day ago</span>
+                    <span>{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-                    quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-                    Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-                    eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-                    error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-                    impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-                    odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos!
-                    <br />
-                    <br />
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-                    quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-                    Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-                    eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-                    error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-                    impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-                    odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+                    {post.desc}
                 </p>
             </div>
         </div>
