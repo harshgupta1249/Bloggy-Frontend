0a2d41fb05d3d16f7e5b8795966e6f1aa886e549
Harsh <harshgupta1249@gmail.com>
2021-11-14 10:54:30 +0530

    login bug fixed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,49 +1,59 @@
 import axios from "axios";
 import { useContext, useRef } from "react";
-import { Context } from "../../context/Context";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./Login.css";
 
+export default function Login() {
 
-const Login = () => {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching, user } = useContext(Context);
 
-    const userRef = useRef();
-    const passwordRef = useRef();
-    const { dispatch, isFetching, user } = useContext(Context);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("http://localhost:8000/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      console.log(res);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+        dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        dispatch({ type: "LOGIN_START" });
-        console.log(isFetching);
-        try {
-            const res = await axios.post("http://localhost:8000/auth/login", {
-                username: userRef.current.value,
-                password: passwordRef.current.value,
-            });
-            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-        } catch (err) {
-            dispatch({ type: "LOGIN_FAILURE" });
-        }
+console.log(user);
 
-        console.log(user);
-        
-    };
-
-    return (
-        <div className="login">
-            <span className="loginTitle">Login</span>
-            <form className="loginForm" onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input className="loginInput" type="text" required placeholder="Enter your username..." ref={userRef} />
-                <label>Password</label>
-                <input className="loginInput" type="password" required placeholder="Enter your password..." ref={passwordRef} />
-                <button className="loginButton" type="submit">Login</button>
-            </form>
-            <button className="loginRegisterButton">
-                <Link className="link" to="/register">REGISTER</Link>
-            </button>
-        </div>
-    );
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your username..."
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit" >
+          Login
+        </button>
+      </form>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
+    </div>
+  );
 }
-
-export default Login;
